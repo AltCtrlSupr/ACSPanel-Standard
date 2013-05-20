7d373a4cba270cbba56c3c7815c2fd56d41d9db5
Genar <gtrias@gmail.com>
2013-05-20 03:09:21 +0200

    created menu for wordpress

---
+++ src/ACS/ACSPanelWordpressBundle/Event/Subscribers/WordpressMenuSubscriber.php
@@ -0,0 +1,28 @@
+<?php
+namespace ACS\ACSPanelWordpressBundle\Event\Subscribers;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use ACS\ACSPanelBundle\Event\FilterMenuEvent;
+
+class WordpressMenuSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'menu.admin.after.items'     => array(
+                array('wordpressItems', 10),
+            ),
+            'menu.user.after.items'     => array(
+                array('wordpressItems', 10),
+            )
+
+        );
+    }
+
+    public function wordpressItems(FilterMenuEvent $menu_filter)
+    {
+        $menu = $menu_filter->getMenu();
+        // TODO: Change the name of settings action
+        $menu->addChild('Wordpress', array( 'route' => 'acsacs_panel_wordpress_list'));
+    }
+}
