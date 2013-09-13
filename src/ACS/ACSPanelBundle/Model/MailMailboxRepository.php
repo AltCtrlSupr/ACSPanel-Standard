2f1dd376260a43d8f486a4ee4a77b6b4eaf5b9f6
Genar <gtrias@gmail.com>
2013-09-13 16:55:23 +0200

    Fixed mailbox user listing issue

--- src/ACS/ACSPanelBundle/Model/MailMailboxRepository.php
+++ src/ACS/ACSPanelBundle/Model/MailMailboxRepository.php
@@ -16,6 +16,13 @@ class MailMailboxRepository extends EntityRepository
         $query = $this->_em->createQuery('SELECT m FROM ACS\ACSPanelBundle\Entity\MailMailbox m INNER JOIN m.mail_domain md INNER JOIN md.domain d WHERE d.user = ?1')->setParameter(1, $user->getId());
         return $query->getResult();
     }
+
+
+    public function findByUsers(Array $user)
+    {
+        $query = $this->_em->createQuery('SELECT h FROM ACS\ACSPanelBundle\Entity\MailMailbox m INNER JOIN m.mail_domain md INNER JOIN md.domain dd WHERE d.user IN (?1)')->setParameter(1, $user);
+        return $query->getResult();
+    }
 }
 
 ?>
