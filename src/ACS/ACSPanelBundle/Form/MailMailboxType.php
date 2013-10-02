ecc06dec4c4389ac823c4b6456ea6c4ac2a1a96c
genar <genar@acs.li>
2013-10-02 19:17:48 +0200

    Fixing maildomain listing

--- src/ACS/ACSPanelBundle/Form/MailMailboxType.php
+++ src/ACS/ACSPanelBundle/Form/MailMailboxType.php
@@ -5,16 +5,46 @@ namespace ACS\ACSPanelBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class MailMailboxType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        // TODO: Do the addition of fields with suscriber
+        global $kernel;
+
+        if ('AppCache' == get_class($kernel)) {
+            $kernel = $kernel->getKernel();
+        }
+        $security = $kernel->getContainer()->get('security.context');
+        $user = $security->getToken()->getUser();
+        $child_ids = $user->getIdChildIds();
+        $superadmin = false;
+        if($security->isGranted('ROLE_SUPER_ADMIN'))
+            $superadmin = true;
+
         $builder
-            ->add('mail_domain')
-            ->add('name')
-            ->add('username')
-            ->add('password')
+            ->add('mail_domain','entity',array(
+                'label' => 'mailbox.form.mail_domain',
+                'class' => 'ACS\ACSPanelBundle\Entity\MailDomain',
+                'query_builder' => function(EntityRepository $er) use ($child_ids, $superadmin){
+                    $query = $er->createQueryBuilder('md')
+                        ->select('md');
+                        //->where('d.is_httpd_alias != 1');
+                        if(!$superadmin){
+                            $query->innerJoin('md.domain','d');
+                            $query->where('d.user IN (?1)')
+                            ->setParameter('1', $child_ids);
+                        }
+                        return $query;
+                    }
+                )
+            )
+
+            ->add('name', null, array('label' => 'mailbox.form.name'))
+            ->add('username', null, array('label' => 'mailbox.form.username'))
+            ->add('password', null, array('label' => 'mailbox.form.password'))
         ;
     }
 
