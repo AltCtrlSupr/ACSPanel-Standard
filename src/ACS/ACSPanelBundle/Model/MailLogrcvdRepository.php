747fdd7f6b4816880fe5f9a64b060860a471451a
genar <genar@acs.li>
2013-10-09 13:53:21 +0200

    Add order to mail rcvd logs

--- src/ACS/ACSPanelBundle/Model/MailLogrcvdRepository.php
+++ src/ACS/ACSPanelBundle/Model/MailLogrcvdRepository.php
@@ -13,7 +13,7 @@ class MailLogrcvdRepository extends EntityRepository
 {
     public function findByUsers(Array $user)
     {
-        $query = $this->_em->createQuery('SELECT ml FROM ACS\ACSPanelBundle\Entity\MailLogrcvd ml INNER JOIN ml.mail_domain md WHERE md.user IN (?1)')->setParameter(1, $user);
+        $query = $this->_em->createQuery('SELECT ml FROM ACS\ACSPanelBundle\Entity\MailLogrcvd ml INNER JOIN ml.mail_domain md WHERE md.user IN (?1) ORDER BY ml.created_at DESC')->setParameter(1, $user);
         return $query->getResult();
     }
 
