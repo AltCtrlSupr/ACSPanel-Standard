9164d6dcab781b880536c8867ba6777a3c7d6770
genar <genar@acs.li>
2014-03-12 12:18:45 +0100

    Fixed form error

--- src/ACS/ACSPanelBundle/Form/DnsRecordType.php
+++ src/ACS/ACSPanelBundle/Form/DnsRecordType.php
@@ -5,6 +5,7 @@ namespace ACS\ACSPanelBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Doctrine\ORM\EntityRepository;
 
 class DnsRecordType extends AbstractType
 {
