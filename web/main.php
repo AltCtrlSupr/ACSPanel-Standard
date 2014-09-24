638c711a182882904dcfe5943eee08d651f5d116
Genar <genar@acs.li>
2014-09-24 14:13:22 +0200

    Welcome back standard app folder structure

--- web/main.php
+++ web/main.php
@@ -12,7 +12,7 @@ $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
 */
 
-require_once __DIR__.'/../app/main/MainKernel.php';
+require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
