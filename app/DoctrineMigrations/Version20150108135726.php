74fe29e7a6a9f5fdaed3768213e0f8d63d5cbb39
Genar Trias <g.trias@picmedia.es>
2015-01-15 19:51:22 +0100

    Fixed Migrations to create ACEs

--- app/DoctrineMigrations/Version20150108135726.php
+++ app/DoctrineMigrations/Version20150108135726.php
@@ -10,8 +10,9 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use Symfony\Component\Security\Acl\Permission\MaskBuilder;
 
+use ACS\ACSPanelBundle\Entity\HttpdHost;
+
 /**
- * Auto-generated Migration: Please modify to your needs!
  */
 class Version20150108135726 extends AbstractMigration implements ContainerAwareInterface
 {
@@ -28,23 +29,174 @@ class Version20150108135726 extends AbstractMigration implements ContainerAwareI
 		$aclManager = $this->container->get('problematic.acl_manager');
 		$em = $this->container->get('doctrine.orm.entity_manager');
 
-		$domains = $em->getRepository('ACS\ACSPanelBundle\Entity\Domain')->findAll();
-		foreach($domains as $domain){
-			$user = $domain->getUser();
-			$aclManager->addObjectPermission($domain, MaskBuilder::MASK_OWNER, $user);
-		}
+        // Adding master permissions to superadmins
+		$superadmins = $em->getRepository('ACS\ACSPanelUsersBundle\Entity\FosUser')->getSuperadminUsers();
+
+        $entity_classes = array('DatabaseUser', 'DB', 'DnsDomain', 'DnsRecord', 'Domain', 'FieldType', 'FtpdUser', 'HttpdHost', 'HttpdUser', 'IpAddress', 'MailAlias', 'MailDomain', 'MailLogrcvd', 'MailMailbox', 'MailWBList', 'PanelSetting', 'Plan', 'Server', 'Service', 'ServiceType', 'UserPlan');
+
+        foreach ($entity_classes as $eclass){
+            $class_rep = "ACS\ACSPanelBundle\Entity\\$eclass";
+            $entities = $em->getRepository($class_rep)->findAll();
+            foreach($entities as $entity){
+
+				$domain_related_user_classes = array('HttpdHost', 'DnsDomain', 'MailAlias');
+				foreach($domain_related_user_classes as $class){
+					if($entity instanceof $class){
+						$user = $entity->getDomain()->getUser();
+						$this->addUserOwnerPermission($user, $entity);
+					}
+				}
+
+				$first_level_user_classes = array('DB', 'DatabaseUser', 'Domain', 'FtpdUser', 'IpAddress', 'LogItem', 'MailDomain', 'MailWBList', 'PanelSetting', 'Server', 'Service');
+				foreach($first_level_user_classes as $class){
+					if($entity instanceof $class){
+						$user = $entity->getUser();
+						$this->addUserOwnerPermission($user, $entity);
+					}
+				}
+
+				if($entity instanceof DnsRecord){
+					$user = $entity->getDnsDomain()->getDomain()->getUser();
+					$this->addUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof HttpdUser){
+					$user = $entity->getHttpdHost()->getDomain()->getUser();
+					$this->addUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof MailLogrcvd){
+					$user = $entity->getMailDomain()->getUser();
+					$this->addUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof MailMailBox){
+					$user = $entity->getMailDomain()->getUser();
+					$this->addUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof UserPlan){
+					$user = $entity->getPuser();
+					$this->addUserOwnerPermission($user, $entity);
+				}
+
+
+                foreach($superadmins as $superadmin){
+                    $aclManager->addObjectPermission($entity, MaskBuilder::MASK_MASTER, $superadmin);
+                }
+
+            }
+        }
+
+        $entity_classes = array('FosUser', 'FosGroup');
+
+        foreach ($entity_classes as $eclass){
+            $class_rep = "ACS\ACSPanelUsersBundle\Entity\\$eclass";
+            $entities = $em->getRepository($class_rep)->findAll();
+            foreach($entities as $entity){
+                foreach($superadmins as $superadmin){
+                    $aclManager->addObjectPermission($entity, MaskBuilder::MASK_MASTER, $superadmin);
+                }
+            }
+        }
+
     }
+
+	public function addUserOwnerPermission($user, $entity)
+	{
+		$aclManager = $this->container->get('problematic.acl_manager');
+
+		if($parent = $user->getParentUser())
+			$aclManager->addObjectPermission($entity, MaskBuilder::MASK_MASTER, $parent);
+
+		$aclManager->addObjectPermission($entity, MaskBuilder::MASK_OWNER, $user);
+	}
 
     public function down(Schema $schema)
     {
 		$aclManager = $this->container->get('problematic.acl_manager');
 		$em = $this->container->get('doctrine.orm.entity_manager');
 
-		$domains = $em->getRepository('ACS\ACSPanelBundle\Entity\Domain')->findAll();
-		foreach($domains as $domain){
-			$user = $domain->getUser();
-			$aclManager->revokePermission($domain, MaskBUILDER::MASK_OWNER, $user);
-		}
+        // Adding master permissions to superadmins
+		$superadmins = $em->getRepository('ACS\ACSPanelUsersBundle\Entity\FosUser')->getSuperadminUsers();
 
+        $entity_classes = array('DatabaseUser', 'DB', 'DnsDomain', 'DnsRecord', 'Domain', 'FieldType', 'FtpdUser', 'HttpdHost', 'HttpdUser', 'IpAddress', 'MailAlias', 'MailDomain', 'MailLogrcvd', 'MailMailbox', 'MailWBList', 'PanelSetting', 'Plan', 'Server', 'Service', 'ServiceType', 'UserPlan');
+
+        foreach ($entity_classes as $eclass){
+            $class_rep = "ACS\ACSPanelBundle\Entity\\$eclass";
+            $entities = $em->getRepository($class_rep)->findAll();
+            foreach($entities as $entity){
+
+				$domain_related_user_classes = array('HttpdHost', 'DnsDomain', 'MailAlias');
+				foreach($domain_related_user_classes as $class){
+					if($entity instanceof $class){
+						$user = $entity->getDomain()->getUser();
+						$this->removeUserOwnerPermission($user, $entity);
+					}
+				}
+
+				$first_level_user_classes = array('DB', 'DatabaseUser', 'Domain', 'FtpdUser', 'IpAddress', 'LogItem', 'MailDomain', 'MailWBList', 'PanelSetting', 'Server', 'Service');
+				foreach($first_level_user_classes as $class){
+					if($entity instanceof $class){
+						$user = $entity->getUser();
+						$this->removeUserOwnerPermission($user, $entity);
+					}
+				}
+
+				if($entity instanceof DnsRecord){
+					$user = $entity->getDnsDomain()->getDomain()->getUser();
+					$this->removeUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof HttpdUser){
+					$user = $entity->getHttpdHost()->getDomain()->getUser();
+					$this->removeUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof MailLogrcvd){
+					$user = $entity->getMailDomain()->getUser();
+					$this->removeUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof MailMailBox){
+					$user = $entity->getMailDomain()->getUser();
+					$this->removeUserOwnerPermission($user, $entity);
+				}
+
+				if($entity instanceof UserPlan){
+					$user = $entity->getPuser();
+					$this->removeUserOwnerPermission($user, $entity);
+				}
+
+
+                foreach($superadmins as $superadmin){
+                    $aclManager->removeObjectPermission($entity, MaskBuilder::MASK_MASTER, $superadmin);
+                }
+
+            }
+        }
+
+        $entity_classes = array('FosUser', 'FosGroup');
+
+        foreach ($entity_classes as $eclass){
+            $class_rep = "ACS\ACSPanelUsersBundle\Entity\\$eclass";
+            $entities = $em->getRepository($class_rep)->findAll();
+            foreach($entities as $entity){
+                foreach($superadmins as $superadmin){
+					$aclManager->revokePermission($entity, MaskBuilder::MASK_MASTER, $superadmin);
+                }
+            }
+        }
     }
+
+	public function removeUserOwnerPermission($user, $entity)
+	{
+		$aclManager = $this->container->get('problematic.acl_manager');
+
+		if($parent = $user->getParentUser())
+			$aclManager->revokePermission($entity, MaskBuilder::MASK_MASTER, $parent);
+
+		$aclManager->revokePermission($entity, MaskBuilder::MASK_OWNER, $user);
+	}
+
 }
