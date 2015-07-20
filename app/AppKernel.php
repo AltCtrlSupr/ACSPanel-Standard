bdfb86d93350dbff1beb5d4a57515694b6d697ed
Genar Trias Ortiz <genar@acs.li>
2015-07-20 10:15:32 +0200

    Added new bundle skeleton to handle billing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -35,6 +35,7 @@ class AppKernel extends Kernel
             new ACS\ACSPanelUsersBundle\ACSACSPanelUsersBundle(),
             new ACS\ACSPanelSettingsBundle\ACSACSPanelSettingsBundle(),
             new ACS\ACSPanelWordpressBundle\ACSACSPanelWordpressBundle(),
+            new ACS\ACSPanelBillingBundle\ACSACSPanelBillingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
