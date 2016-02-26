bab3519fe148bd30bbffb411683c316c16977aca
root <root@debian>
2016-02-26 13:11:29 -0600

    some minor fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,12 @@ cd /var/www
 if [ -z "$1" ];
     then
     composer install
-    php app/console avanzu:admin:fetch-vendor
+    php app/console avanzu:admin:fetch-vendor --root
     rm -rf app/cache/*
     php app/console assets:install --symlink web/
     php app/console c:c
     php app/console c:w
-    service php7-fpm start
+    service php7.0-fpm start
     nginx
 else
     exec "$@"
