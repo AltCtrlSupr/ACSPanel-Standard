2e82816f0ff4a05858c84c43cbc9f9eb500f3bbb
Genar Trias Ortiz <genar@acs.li>
2016-08-02 10:05:21 +0200

    entityrepositories service definition fixes

--- src/ACS/ACSPanelUsersBundle/Doctrine/AclEntityRepository.php
+++ src/ACS/ACSPanelUsersBundle/Doctrine/AclEntityRepository.php
@@ -8,11 +8,24 @@ class AclEntityRepository extends EntityRepository
 {
     private $acl_filter;
 
+    /**
+     * setAclFilter
+     *
+     * @param mixed $acl_filter
+     * @access public
+     * @return void
+     */
     public function setAclFilter($acl_filter)
     {
         $this->acl_filter = $acl_filter;
     }
 
+	/**
+	 * getAclFilter
+	 *
+	 * @access public
+	 * @return void
+	 */
 	public function getAclFilter()
 	{
         return $this->acl_filter;
